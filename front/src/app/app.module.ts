5cb1cca77476f7f501b3a6638eb7c9c686c84325
Juanjo78git <juanjo78@gmail.com>
2021-08-28 12:24:40 +0200

    Renombrar Componente Question-list

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './components/question/question.component';
 import { QuestionCreateComponent } from './components/question-create/question-create.component';
-import { QuestionDetailComponent } from './components/question-detail/question-detail.component';
 import { QuizDetailComponent } from './components/quiz-detail/quiz-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    QuestionComponent,
     QuestionCreateComponent,
-    QuestionDetailComponent,
     QuizDetailComponent
   ],
   imports: [
