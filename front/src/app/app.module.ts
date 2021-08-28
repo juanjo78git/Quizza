489ea9c954bb61655abbe9bef814ad24d972733d
Juanjo78git <juanjo78@gmail.com>
2021-08-28 12:11:39 +0200

    Creación de componentes

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionCreateComponent } from './components/question-create/question-create.component';
+import { QuestionDetailComponent } from './components/question-detail/question-detail.component';
+import { QuizDetailComponent } from './components/quiz-detail/quiz-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionCreateComponent,
+    QuestionDetailComponent,
+    QuizDetailComponent
   ],
   imports: [
     BrowserModule,
