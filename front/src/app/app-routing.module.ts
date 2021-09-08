fa3df7b165c47d6407c25b616cbbb4cea54f74cb
Juanjo78git <juanjo78@gmail.com>
2021-09-08 18:24:41 +0200

    routing v1

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BookShowComponent } from './cyoabook/book-show/book-show.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'book', component: BookShowComponent },
+  { path: 'book/:bookId', component: BookShowComponent },
+  { path: '**', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
